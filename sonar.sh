f22210fef575a283419e2eae39146a08063ba64d
Mihail Mihalache <mihalache_mihail@yahoo.com>
2018-01-28 23:48:03 +0200

    #1 adding sonar files

---
+++ sonar.sh
@@ -0,0 +1,9 @@
+build-wrapper-linux-x86-64 --out-dir bw-output make clean all
+
+sonar-scanner \
+    -Dsonar.projectKey=$SONAR_PROJECT \
+    -Dsonar.sources=. \
+    -Dsonar.cfamily.build-wrapper-output=bw-output \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=$SONAR_ORG \
+    -Dsonar.login=$SONAR_USER
